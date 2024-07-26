3a788895ccfa3d3ecf32ffc8a225cd5b1b333886
Bharath Ravuri <bharathnaidu652@gmail.com>
2024-07-26 14:19:12 -0500

    Commiting changes to App.js
    Including the basic Voice chat functionality

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
-const port = 3000;
+const twilio = require('twilio');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.post('/voice', (req, res) => {
+  const VoiceResponse = twilio.twiml.VoiceResponse;
+  const twiml = new VoiceResponse();
+  
+  console.log('Incoming call received');
+  
+  twiml.say('Hello, this is your deployed server. The voice assistant is working!');
+  
+  res.type('text/xml');
+  res.send(twiml.toString());
 });
 
 app.listen(port, () => {
